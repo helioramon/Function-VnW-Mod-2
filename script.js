5f387a605afcb5f7a940ee146749a1674b3e5d13
helioramon <helio.alvareng@gmail.com>
2022-06-21 02:10:36 -0300

    Applying function

---
+++ script.js
@@ -0,0 +1,32 @@
+function mandarMensagem(){
+    console.log("Sejam bem vindos")
+}
+
+mandarMensagem()
+
+function helloFriend(nome) {
+    console.log(nome)
+}
+
+helloFriend("Hélio Ramon Alvarenga Ferreira")
+
+function enviarInformações (nome, idade, estilo){
+    console.log(nome)
+    console.log(idade)
+    console.log(estilo)
+}
+
+enviarInformações('Hélio', '27 anos', 'Rock')
+
+function enviarTitulos (filme, música){
+    console.log(filme)
+    console.log(música)
+}
+
+enviarTitulos('Oasis', 'Frou Frou foxes in Midsummer Fires')
+
+function enviarTriplo (valor){
+    return valor* 3
+}
+
+console.log(enviarTriplo(20))
